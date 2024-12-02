7f27fbb91ca8c08cb74d5b5cc55dbb911f680a91
Billy Sheppard <billy.sheppard@rmhedge.com>
2024-12-02 06:16:04 +0000

    updates

--- src/lib.rs
+++ src/lib.rs
@@ -20,10 +20,10 @@ pub fn initialise() -> Result<CountryHolidayMap, String> {
     ron::de::from_bytes(&out).map_err(|e| format!("{e:?}"))
 }
 
-#[cfg(all(feature = "years", not(target_arch = "wasm_32")))]
+#[cfg(all(feature = "years", not(target_arch = "wasm32")))]
 pub use years::*;
 
-#[cfg(all(feature = "years", not(target_arch = "wasm_32")))]
+#[cfg(all(feature = "years", not(target_arch = "wasm32")))]
 mod years {
     use std::{io::Write, process::Stdio};
 
@@ -32,7 +32,7 @@ mod years {
     const SCRIPT: &str = include_str!("../gen_objects.py");
 
     pub fn generate_with_years(years: Vec<usize>) -> Result<CountryHolidayMap, String> {
-        let script = SCRIPT.replace("[2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030]", &format!("{years:?}"));
+        let script = SCRIPT.replace("[2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024,2025,2026,2027,2028,2029,2030,2031,2032,2033,2034,2035,2036,2037,2038,2039,2040,2041,2042,2043,2044,2045,2046,2047,2048,2049,2050]", &format!("{years:?}"));
 
         let mut process = std::process::Command::new("python")
             .arg("-")
@@ -71,7 +71,7 @@ mod tests {
     }
 
     #[test]
-    #[cfg(all(feature = "years", not(target_arch = "wasm_32")))]
+    #[cfg(all(feature = "years", not(target_arch = "wasm32")))]
     fn test_generate_with_years() {
         assert!(generate_with_years(Vec::from([2023])).is_ok());
     }

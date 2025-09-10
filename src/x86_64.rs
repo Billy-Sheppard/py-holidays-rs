291d41030e4805fd076b752e57af7a4708493d55
Billy Sheppard <billy.sheppard@rmhedge.com>
2025-09-10 00:04:46 +0000

    0.1.3

--- src/x86_64.rs
+++ src/x86_64.rs
@@ -2,30 +2,15 @@ use std::io::Read;
 
 #[allow(dead_code)]
 pub(crate) fn get(url: impl AsRef<str>) -> Result<Vec<u8>, String> {
-    let rt = tokio::runtime::Runtime::new().map_err(|e| e.to_string())?;
-    let body = rt.block_on(async {
-        let https = hyper_tls::HttpsConnector::new();
-        let client =
-            hyper_util::client::legacy::Client::builder(hyper_util::rt::TokioExecutor::new())
-                .build(https);
+    let future = get_inner(url);
 
-        let req = hyper::Request::builder()
-            .method("GET")
-            .uri(url.as_ref())
-            .body(String::new())
-            .map_err(|e| e.to_string())?;
-
-        let res = client.request(req).await.map_err(|e| e.to_string())?;
-
-        if !res.status().is_success() {
-            return Err(format!("HTTP error: {}", res.status()));
+    let body = match tokio::runtime::Handle::try_current() {
+        Ok(handle) => tokio::task::block_in_place(|| handle.block_on(future)),
+        Err(_) => {
+            let rt = tokio::runtime::Runtime::new().map_err(|e| e.to_string())?;
+            rt.block_on(future)
         }
-        let body_bytes = http_body_util::BodyExt::collect(res.into_body())
-            .await
-            .map_err(|e| e.to_string())?
-            .to_bytes();
-        Ok(body_bytes.to_vec())
-    })?;
+    }?;
 
     let mut deflated = Vec::new();
     flate2::read::GzDecoder::new(body.as_slice())
@@ -33,4 +18,27 @@ pub(crate) fn get(url: impl AsRef<str>) -> Result<Vec<u8>, String> {
         .map_err(|e| e.to_string())?;
 
     Ok(deflated)
+}
+
+async fn get_inner(url: impl AsRef<str>) -> Result<Vec<u8>, String> {
+    let https = hyper_tls::HttpsConnector::new();
+    let client = hyper_util::client::legacy::Client::builder(hyper_util::rt::TokioExecutor::new())
+        .build(https);
+
+    let req = hyper::Request::builder()
+        .method("GET")
+        .uri(url.as_ref())
+        .body(String::new())
+        .map_err(|e| e.to_string())?;
+
+    let res = client.request(req).await.map_err(|e| e.to_string())?;
+
+    if !res.status().is_success() {
+        return Err(format!("HTTP error: {}", res.status()));
+    }
+    let body_bytes = http_body_util::BodyExt::collect(res.into_body())
+        .await
+        .map_err(|e| e.to_string())?
+        .to_bytes();
+    Ok(body_bytes.to_vec())
 }
